8019843f24ca184b2d1083f2d40b8bb1e9ec1c02
douglasAtJoyent <douglas.andeson@joyent.com>
2018-04-27 16:59:41 -0500

    Indentation went strange on the last checkin.

--- src/integration/java/com/joyent/manta/archiver/TransferManagerIT.java
+++ src/integration/java/com/joyent/manta/archiver/TransferManagerIT.java
@@ -1,10 +1,10 @@
-package com.joyent.manta.archiver;/*
-                                   * Copyright (c) 2017, Joyent, Inc. All rights reserved.
-                                   *
-                                   * This Source Code Form is subject to the terms of the Mozilla Public License, v.
-                                   * 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at
-                                   * http://mozilla.org/MPL/2.0/.
-                                   */
+package com.joyent.manta.archiver;
+/*
+ * Copyright (c) 2017, Joyent, Inc. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 import com.joyent.manta.client.MantaClient;
 import org.apache.commons.io.FileUtils;
